37440deb18e0d6cdff615f7045437cabed7e9a37
珅 <ys06000@ly.com>
2020-01-02 13:29:22 +0800

    添加全能转换,允许qx和surge混用代码

---
+++ smart.js
@@ -0,0 +1,111 @@
+/*
+    本作品用于QuantumultX和Surge之间js执行方法的转换
+    您只需书写其中任一软件的js,然后在您的js最后追加上此段js即可
+    无需担心影响执行问题,具体原理是将QX和Surge的方法转换为互相可调用的方法
+    尚未测试是否支持import的方式进行使用,因此暂未export
+    如有问题或您有更好的改进方案,请前往 https://github.com/sazs34/TaskConfig/issues 提交内容,或直接进行pull request
+    您也可直接在tg中联系@wechatu
+*/
+// #region 固定头部
+let isQuantumultX = $task === undefined; //判断当前运行环境是否是qx
+let isSurge = $httpClient === undefined; //判断当前运行环境是否是surge
+// http请求
+let $task = isQuantumultX ? $task : {};
+let $httpClient = isSurge ? $httpClient : {};
+// cookie读写
+let $prefs = isQuantumultX ? $prefs : {};
+let $persistentStore = isSurge ? $persistentStore : {};
+// 消息通知
+let $notify = isQuantumultX ? $notify : {};
+let $notification = isSurge ? $notification : {};
+// #endregion 固定头部
+
+// #region 网络请求专用转换
+if (isQuantumultX) {
+    let errorInfo = {
+        error: ''
+    };
+    $httpClient = {
+        get: (url, cb) => {
+            $task.fetch(url).then(response => {
+                cb(errorInfo, response, response.body)
+            }, reason => {
+                errorInfo.error = reason.error;
+                cb(errorInfo, response, '')
+            })
+        },
+        post: (url, cb) => {
+            url.method = 'POST';
+            $task.fetch(url).then(response => {
+                cb(errorInfo, response, response.body)
+            }, reason => {
+                errorInfo.error = reason.error;
+                cb(errorInfo, response, '')
+            })
+        }
+    }
+}
+if (isSurge) {
+    $task = {
+        fetch: url => {
+            //为了兼容qx中fetch的写法,所以永不reject
+            return new Promise((respove, reject) => {
+                if (url.method == 'POST') {
+                    $httpClient.post(url, (error, response, data) => {
+                        response.body = data;
+                        resolve(response, {
+                            error: error
+                        });
+                    })
+                } else {
+                    $httpClient.get(url, (error, response, data) => {
+                        response.body = data;
+                        resolve(response, {
+                            error: error
+                        });
+                    })
+                }
+            })
+
+        }
+    }
+}
+// #endregion 网络请求专用转换
+
+// #region cookie操作
+if (isQuantumultX) {
+    $persistentStore = {
+        read: key => {
+            return $prefs.valueForKey(key);
+        },
+        write: (val, key) => {
+            return $prefs.setValueForKey(val, key);
+        }
+    }
+}
+if (isSurge) {
+    $prefs = {
+        valueForKey: key => {
+            return $persistentStore.read(key);
+        },
+        setValueForKey: (val, key) => {
+            return $persistentStore.write(val, key);
+        }
+    }
+}
+// #endregion
+
+// #region 消息通知
+if (isQuantumultX) {
+    $notification = {
+        post: (title, subTitle, detail) => {
+            $notify(title, subTitle, detail);
+        }
+    }
+}
+if (isSurge) {
+    $notify = function (title, subTitle, detail) {
+        $notification.post(title, subTitle, detail);
+    }
+}
+// #endregion
